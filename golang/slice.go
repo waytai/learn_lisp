72f09b2d666afb9269ebf3a3163c82b82d11f03f
wayne <278972894@qq.com>
2016-01-22 17:37:31 +0800

    change

---
+++ golang/slice.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+s0 := make([]int, 2, 10)
+fmt.Printf("Element: %d %d\n", s0[0], s0[1])
+s1 := append(s0, 2)
+s2 := append(s0, 3)
+fmt.Printf("Element: %d %d\n", s1[2], s2[2])
+s0 = []int{0, 1}
+s1 = append(s0, 2)
+s2 = append(s0, 3)
+fmt.Printf("Element: %d %d\n", s1[2], s2[2])
+}
