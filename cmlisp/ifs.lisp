e623216e89aaaeafad7edc2612d8765333ce2362
waytai <278972894@qq.com>
2016-03-05 21:01:57 +0800

    change

---
+++ cmlisp/ifs.lisp
@@ -0,0 +1,2 @@
+(setq ss (if (listp 27)
+	(+ 1 2)	))
---
+++ cmlisp/ls.lisp
@@ -0,0 +1,6 @@
+(setq tt (list 'my (+ 2 1)))
+(setq ss (quote (+ 3 5 )))
+(setq sss '(+ 3 5 ))
+
+(setq ls (cons 'a '(b c d)))
+(setq lscar (car '(a b c)))
