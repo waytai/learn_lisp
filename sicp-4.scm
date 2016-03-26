0122e44d8f42e92a7b1b13df22142adf1df52d0e
waytai <278972894@qq.com>
2016-03-26 14:37:18 +0800

    sicp 4 test

---
+++ sicp-4.scm
@@ -0,0 +1,33 @@
+(define (self-evaluating? exp)
+  (cond ((number? exp) true)
+        ((string? exp) true)
+        (else false)))
+
+
+(define (quoted? exp)
+  (tagged-list? exp 'quote))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+;;(display (eval '(* 5 5) user-initial-environment))
+
+;;(display 
+;;    (self-evaluating? '(* 5 5))
+;;  )
+
+(define (variable? exp) (symbol? exp))
+
+(display 
+    (variable? 'a)
+)
+
+(display 
+    (self-evaluating? 5)
+)
+
+(display 
+    (quoted? '(quote 10))
+)
