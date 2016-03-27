503e48280a6fb0d1e67b0d451623e3d52e8c0def
waytai <278972894@qq.com>
2016-03-27 18:34:59 +0800

    add test

--- sicp-4.scm
+++ sicp-4.scm
@@ -27,14 +27,20 @@
     (variable? 'a)
 )
 
+(newline)
+
 (display 
     (self-evaluating? 5)
 )
+
+
+(newline)
 
 (display 
     (quoted? '(quote 10))
 )
 
+(newline)
 
 (display 
     (definition? '(define 10))
@@ -46,3 +52,72 @@
 (display 
     (text-of-quotation '(quote 10))
 )
+
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars)
+             (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (car vals))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+        (error "Unbound variable" var)
+        (let ((frame (first-frame env)))
+          (scan (frame-variables frame)
+                (frame-values frame)))))
+  (env-loop env))
+
+
+(define (enclosing-environment env) (cdr env))
+
+(define the-empty-environment '())
+
+(define (first-frame env) (car env))
+
+(define (frame-values frame) (cdr frame))
+
+(define (frame-variables frame) (car frame))
+
+(newline)
+
+(define a 234)
+
+(display a)
+
+
+(define a 234)
+
+
+(define (make-frame variables values)
+  (cons variables values))
+
+(define vars '(a b c d))
+(define vars_s '(a b c d))
+(define vals (list 1 2 3 4))
+
+(newline)
+
+(display
+  (equal? vars vars_s)
+  )
+
+(newline)
+
+(display
+  (make-frame vars vals)
+  )
+
+(define a (make-frame vars vals))
+(define env (cons a '()))
+
+(newline)
+(display
+  (lookup-variable-value 'a env)
+  )
+
+
+
+
+
