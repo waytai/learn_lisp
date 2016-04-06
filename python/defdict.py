75af45c29e9fcfd2ea2d4cd2fbbbf98aa520eaf2
wayne <278972894@qq.com>
2016-04-06 17:25:18 +0800

    default dict

---
+++ python/defdict.py
@@ -0,0 +1,23 @@
+#########################################################################
+#-*- coding:utf-8 -*-
+# File Name: test.py
+#########################################################################
+#!/bin/python
+
+from collections import defaultdict
+
+colours = (
+    ('Yasoob', 'Yellow'),
+    ('Ali', 'Blue'),
+    ('Arham', 'Green'),
+    ('Ali', 'Black'),
+    ('Yasoob', 'Red'),
+    ('Ahmed', 'Silver'),
+)
+
+favourite_colours = defaultdict(list)
+
+for name, colour in colours:
+    favourite_colours[name].append(colour)
+
+print favourite_colours
